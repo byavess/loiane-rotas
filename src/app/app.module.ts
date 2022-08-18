63a28df460f77c88bb161711ed22687ccb9a435d
famila <famila@pop-os.localdomain>
2022-08-18 11:54:36 -0300

    TESTE STORYBOOK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,21 @@
-import { AlunosModule } from './alunos/alunos.module';
+
 
 //import { CursosService } from './cursos/cursos.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { HomeComponent } from './home/home.component';
 //import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
-//import { HttpModule } from '@angular/http';
-import { MaterializeButtonModule , MaterializeCardModule } from 'materialize-angular'
 
-//import { routing } from './app.routing';
+import { routing } from './app.routing';
 //import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 //import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app.routing.module';
-import { CursosModule } from './cursos/cursos.module';
+
 
 @NgModule({
   declarations: [
@@ -33,10 +31,9 @@ import { CursosModule } from './cursos/cursos.module';
     BrowserModule,
     FormsModule,
    AppRoutingModule,
-   // routing,
-  // HttpModule,
-   CursosModule,
-   AlunosModule
+   routing,
+
+   
     
   ],
   //providers: [CursosService],
---
+++ src/stories/login.component.stories.ts
@@ -0,0 +1,10 @@
+import { HomeComponent } from "src/app/home/home.component";
+import { LoginComponent } from "src/app/login/login.component";
+
+export default{
+    title:'login',
+   component: LoginComponent
+};
+export const Default = () =>({
+    component: HomeComponent
+})
